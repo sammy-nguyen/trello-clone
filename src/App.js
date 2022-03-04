f4d0f594ab4592cebf44948be381bc615a74ed51
sammy-nguyen <Sambu1227@gmail.com>
2022-03-03 18:44:47 -0700

    added trello-clone 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World</p>
+    <div className="home">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2PwRQttmK18ZBYWADrccj8tjjsnIWtDFvqQ&usqp=CAU"
+                alt="JavaScript-img"
+              ></img>
+              Title: JavaScript's course
+            </li>
+            <li>add what you like</li>
+            <li>add what you like</li>
+            <li>add what you like</li>
+            <li>add what you like</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2PwRQttmK18ZBYWADrccj8tjjsnIWtDFvqQ&usqp=CAU"
+                alt="JavaScript-img"
+              ></img>
+              Title: JavaScript's course
+            </li>
+            <li>add what you like</li>
+            <li>add what you like</li>
+            <li>add what you like</li>
+            <li>add what you like</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2PwRQttmK18ZBYWADrccj8tjjsnIWtDFvqQ&usqp=CAU"
+                alt="JavaScript-img"
+              ></img>
+              Title: JavaScript's course
+            </li>
+            <li>add what you like</li>
+            <li>add what you like</li>
+            <li>add what you like</li>
+            <li>add what you like</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
